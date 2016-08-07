2a8ae31d4dc4eff37d99f7eb4295f26d97158c8e
Alexandr Sachkov <alexsachkov@hotmail.com>
2016-08-07 01:22:30 -0700

    rendering engine now exposes full and system interfaces

--- NLE/NL_IRenderingEngine.h
+++ NLE/NL_IRenderingEngine.h
@@ -1,21 +1,27 @@
 #ifndef NL_I_RENDERING_ENGINE_H_
 #define NL_I_RENDERING_ENGINE_H_
 
+#include "NL_ISystem.h"
 #include "NL_CommonTypes.h"
 #include <cstdint>
 #include <string>
 
-struct ID3D11Device;
 namespace NLE
 {
 	namespace GRAPHICS
 	{
-		class IRenderingEngine
+		class IRenderingEngine_SysService
 		{
 		public:
 			virtual void setWindowTitle(std::wstring title) = 0;
 			virtual void setResolution(Size2D resolution) = 0;
 			virtual void setFullscreen(bool fullscreen) = 0;
+		};
+
+		class IRenderingEngine : public ISystem, public IRenderingEngine_SysService
+		{
+		public:
+			virtual bool initialize() = 0;
 		};
 	}
 }
